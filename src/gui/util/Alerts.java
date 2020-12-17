351be3d67de715d1a451bf2e32c2a8ca1eea7762
Edinardo Freitas Filho <edinardofreitas@gmail.com>
2020-12-17 08:58:09 -0400

    1.1.0 Version

--- src/gui/util/Alerts.java
+++ src/gui/util/Alerts.java
@@ -2,6 +2,7 @@ package gui.util;
 
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.stage.Modality;
 
 public class Alerts {
 	public static void showAlert(String title, String header, String content, AlertType type) {
--- src/model/services/Gradient.java
+++ src/model/services/Gradient.java
@@ -4,7 +4,6 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 import gui.util.Alerts;
-import javafx.event.ActionEvent;
 
 public class Gradient {
 
@@ -12,65 +11,63 @@ public class Gradient {
 	private double finalValue;
 	private double rate;
 
-	private int totalTimeHours;
-	private int totalTimeMinutes;
-	private int totalTimeSeconds;
 	private int alarmHours;
 	private int alarmMinutes;
 	private int alarmSeconds;
-	
-	private static String info;
-	private static String value;
-	private static String time;
 
 	private static SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-
 
 	public Gradient(double initialValue, double finalValue, int totalTimeHours, int totalTimeMinutes,
 			int totalTimeSeconds, int alarmHours, int alarmMinutes, int alarmSeconds) {
 		this.realTimeValue = initialValue;
 		this.finalValue = finalValue;
-		this.totalTimeHours = totalTimeHours;
-		this.totalTimeMinutes = totalTimeMinutes;
-		this.totalTimeSeconds = totalTimeSeconds;
 		this.alarmHours = alarmHours;
 		this.alarmMinutes = alarmMinutes;
 		this.alarmSeconds = alarmSeconds;
-		this.rate = (this.realTimeValue - this.finalValue) / ((totalTimeHours * 60 * 60) + (totalTimeMinutes * 60) + totalTimeSeconds);
+		this.rate = (this.realTimeValue - this.finalValue)
+				/ ((totalTimeHours * 60 * 60) + (totalTimeMinutes * 60) + totalTimeSeconds);
 	}
-
-	
 
 	public Gradient(double initialValue, double finalValue, double rate, int alarmHours, int alarmMinutes,
 			int alarmSeconds) {
 		this.realTimeValue = initialValue;
 		this.finalValue = finalValue;
-		this.rate = rate;
+		this.rate = rate / (60 * 60);
 		this.alarmHours = alarmHours;
 		this.alarmMinutes = alarmMinutes;
 		this.alarmSeconds = alarmSeconds;
 	}
 
-
 	public void countingGradient(String info, String unit) {
 		Calendar now = Calendar.getInstance();
-		Calendar totalTime = Calendar.getInstance();
-		totalTime.add(Calendar.HOUR_OF_DAY, totalTimeHours);
-		totalTime.add(Calendar.MINUTE, totalTimeMinutes);
-		totalTime.add(Calendar.SECOND, totalTimeSeconds);
-		
-		while (now.getTimeInMillis() < totalTime.getTimeInMillis()) {
-			Calendar alarm = Calendar.getInstance();
-			alarm.add(Calendar.HOUR_OF_DAY, alarmHours);
-			alarm.add(Calendar.MINUTE, alarmMinutes);
-			alarm.add(Calendar.SECOND, alarmSeconds);
-			while (now.getTimeInMillis() < alarm.getTimeInMillis()) {
-				now = Calendar.getInstance();
+
+		if (realTimeValue > finalValue) {
+			while (realTimeValue > finalValue) {
+				Calendar alarm = Calendar.getInstance();
+				alarm.add(Calendar.HOUR_OF_DAY, alarmHours);
+				alarm.add(Calendar.MINUTE, alarmMinutes);
+				alarm.add(Calendar.SECOND, alarmSeconds);
+				while (now.getTimeInMillis() < alarm.getTimeInMillis()) {
+					now = Calendar.getInstance();
+				}
+				realTimeValue -= rate * ((alarmHours * 60 * 60) + (alarmMinutes * 60) + alarmSeconds);
+				Alerts.showGradient(info, (String.format("%.2f", realTimeValue) + unit), sdf.format(now.getTime()));
 			}
-			realTimeValue -= rate * ((alarmHours * 60 * 60) + (alarmHours * 60) + alarmSeconds);
+			Alerts.showGradient(info, (String.format("%.2f", realTimeValue) + unit), sdf.format(now.getTime()));
+		} else {
+			while (realTimeValue < finalValue) {
+				Calendar alarm = Calendar.getInstance();
+				alarm.add(Calendar.HOUR_OF_DAY, alarmHours);
+				alarm.add(Calendar.MINUTE, alarmMinutes);
+				alarm.add(Calendar.SECOND, alarmSeconds);
+				while (now.getTimeInMillis() < alarm.getTimeInMillis()) {
+					now = Calendar.getInstance();
+				}
+				realTimeValue -= rate * ((alarmHours * 60 * 60) + (alarmMinutes * 60) + alarmSeconds);
+				Alerts.showGradient(info, (String.format("%.2f", realTimeValue) + unit), sdf.format(now.getTime()));
+			}
 			Alerts.showGradient(info, (String.format("%.2f", realTimeValue) + unit), sdf.format(now.getTime()));
 		}
-		Alerts.showGradient(info, (String.format("%.2f", realTimeValue) + unit), sdf.format(now.getTime()));
 	}
 
 	public void countingRate(String info, String unit) {
@@ -85,7 +82,7 @@ public class Gradient {
 				while (now.getTimeInMillis() < alarm.getTimeInMillis()) {
 					now = Calendar.getInstance();
 				}
-				realTimeValue -= rate * ((alarmHours * 60 * 60) + (alarmHours * 60) + alarmSeconds);
+				realTimeValue -= rate * ((alarmHours * 60 * 60) + (alarmMinutes * 60) + alarmSeconds);
 				Alerts.showGradient(info, (String.format("%.2f", realTimeValue) + unit), sdf.format(now.getTime()));
 			}
 			Alerts.showGradient(info, (String.format("%.2f", realTimeValue) + unit), sdf.format(now.getTime()));
@@ -98,7 +95,7 @@ public class Gradient {
 				while (now.getTimeInMillis() < alarm.getTimeInMillis()) {
 					now = Calendar.getInstance();
 				}
-				realTimeValue += rate * ((alarmHours * 60 * 60) + (alarmHours * 60) + alarmSeconds);
+				realTimeValue += rate * ((alarmHours * 60 * 60) + (alarmMinutes * 60) + alarmSeconds);
 				Alerts.showGradient(info, (String.format("%.2f", realTimeValue) + unit), sdf.format(now.getTime()));
 			}
 			Alerts.showGradient(info, (String.format("%.2f", realTimeValue) + unit), sdf.format(now.getTime()));
